58ee3085710852dfe3fc93f35631491a7aa4267a
kyr1234 <yugkhokhar18@gmail.com>
2022-01-13 17:10:28 +0530

    react boilerplate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navabar from './Components/Navbar'
+import Shop from './Components/Shop'
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Navabar />
+      </div>
+      <Shop />
+    </>
+  )
+}
+
+export default App
